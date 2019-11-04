743aee9b30dc598c2809ffcbc4283880ae53f095
Nethelbert <gilblasse@gmail.com>
2019-11-04 18:59:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  arr = array.each_with_index.map do |item,i|
+					i == array.index(array.last) && array.size > 1 ? "and #{item}" : "#{item}"  
+        end
+  arr.length > 2 ? arr.join(', ') : arr.join(' ')
 end
