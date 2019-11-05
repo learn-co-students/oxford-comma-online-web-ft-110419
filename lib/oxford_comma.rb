013c1742c653fafa396f6221a98d749d3af913fc
Jess Fischbach <robotspacefish@gmail.com>
2019-11-05 18:39:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  sentence = array
+    len = array.size
+
+    if len == 1
+      return sentence[0]
+    elsif len == 2
+      sentence = sentence.join(' and ')
+    elsif len > 2
+
+      last = sentence.pop
+      # print sentence
+      sentence = sentence.join(', ')
+      sentence << ', and '
+      sentence << last
+    end
+
+    sentence
 end
