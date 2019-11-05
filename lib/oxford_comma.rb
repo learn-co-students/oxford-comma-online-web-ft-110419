2470265b302e680589636582fcb2c878a5e78340
Kevin Downs <kevin.downs04@gmail.com>
2019-11-05 19:26:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2
+    return array.join(" and ")
+  end
+  last = array.pop
+  string = array.join(", ")
+  string << ", and #{last}"
 end
