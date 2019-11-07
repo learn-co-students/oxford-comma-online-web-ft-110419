3c943045e249f077af654eb84ac272a926562925
Ivana <ivana_pile@yahoo.com>
2019-11-06 22:03:06 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
-
+def oxford_comma(kiwi)
+    ["kiwi"].join
 end
+def oxford_comma
+ ["kiwi", "durian"].join("and")    
+end
+
+def oxford_comma_adds
+
+    ["kiwi", "durian"].join(",") << "and starfruit" 
+    
+end
+
+
+
+
+  #For instance, you can add elements to the end of strings with the* `<<` *("shovel")
+   #method just like you can with arrays.*
